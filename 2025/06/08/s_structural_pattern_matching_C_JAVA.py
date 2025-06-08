1ea04093e23d5714c875ca62c1bd9a4e31147793
misty6760 <kingh0jun12@naver.com>
2025-06-08 17:56:07 +0900

    2025/6/8 add file

---
+++ 2025/06/08/s_structural_pattern_matching_C_JAVA.py
@@ -0,0 +1,11 @@
+# C, Java 스타일
+def switch_case(value):
+    match value:
+        case 1:
+            return "One"
+        case 2:
+            return "Two"
+        case 3:
+            return "Three"
+        case _:
+            return "Unknown"
---
+++ 2025/06/08/s_structural_pattern_matching_py.py
@@ -0,0 +1,17 @@
+def describe_shape(shape):
+    # 튜플 패턴 매칭
+    match shape:
+        case ("circle", r):
+            return f"Circle radius: {r}"
+        case ("square", s):
+            return f"Square side: {s}"
+        case ("rectangle", w,h):
+            return f"Rectangle width: {w}, height: {h}"
+        case _:
+            return f"Unknown shape"
+
+# 사용 예시
+print(describe_shape(("circle", 5)))
+print(describe_shape(("square", 4)))
+print(describe_shape(("rectangle", 3, 6)))
+print(describe_shape(("triangle", 3, 4)))
