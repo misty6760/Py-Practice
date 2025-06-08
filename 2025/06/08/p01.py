9ec7b74d4dbe58ad7b7c07f29df73577c1235e82
misty6760 <kingh0jun12@naver.com>
2025-06-08 14:35:29 +0900

    2025/6/8

---
+++ 2025/06/08/p01.py
@@ -0,0 +1,3 @@
+age = 20
+status = "성인" if age >= 18 else "청소년"
+print(status)
---
+++ 2025/06/08/p02.py
@@ -0,0 +1,5 @@
+name = "John"
+age = 25
+print("My name is %s and I am %d years old." % (name, age))
+print("My name is {} and I am {} years old.".format(name, age))
+print(f"My name is {name} and I am {age} years old.")
